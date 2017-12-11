c6d5085a0100e01e0637c51f01b7836b941d9277
Petter Iversen <iversen332@gmail.com>
2017-12-11 11:15:28 +0100

    remove dist

--- dist/entities/api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MethodName;
-(function (MethodName) {
-    MethodName["GET"] = "GET";
-    MethodName["POST"] = "POST";
-})(MethodName = exports.MethodName || (exports.MethodName = {}));
